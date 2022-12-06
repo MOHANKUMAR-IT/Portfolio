59267b4bbed28748e4a7714c5ef8cf6188809966
MOHAN KUMAR R <mohankumar.it.2001@gmail.com>
2022-12-06 13:51:31 +0530

    modified:   node_modules/.cache/.eslintcache 	modified:   node_modules/.cache/default-development/0.pack 	modified:   node_modules/.cache/default-development/1.pack 	modified:   node_modules/.cache/default-development/10.pack 	modified:   node_modules/.cache/default-development/2.pack 	modified:   node_modules/.cache/default-development/3.pack 	modified:   node_modules/.cache/default-development/4.pack 	modified:   node_modules/.cache/default-development/5.pack 	modified:   node_modules/.cache/default-development/6.pack 	modified:   node_modules/.cache/default-development/7.pack 	modified:   node_modules/.cache/default-development/8.pack 	modified:   node_modules/.cache/default-development/9.pack 	modified:   node_modules/.cache/default-development/index.pack 	modified:   node_modules/.cache/default-development/index.pack.old 	modified:   node_modules/.package-lock.json 	modified:   package-lock.json 	modified:   package.json 	new file:   public/coding-ide.jpg 	new file:   public/home.png 	modified:   public/index.html 	new file:   public/jarvis.png 	new file:   public/smart-home.jpg 	modified:   src/codebase.js

--- src/codebase.js
+++ src/codebase.js
@@ -1,8 +1,11 @@
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 export default function Codebase(){
     const projectInfo = [
         {
-            "img":"http://www.tcedesk.tk:31355/home.png",
+            "img":"/home.png",
             "title":"Customer Care Registry",
             "description":"Customer grievance's solving site built using IBM cloud technology and flask framework.",
             "github-link":"https://github.com/IBM-EPBL/IBM-Project-31539-1660201957",
@@ -13,66 +16,75 @@ export default function Codebase(){
             "title":"Cloud Based Virtula Labs",
             "description":"Cloud virtual labs supported by docker backend and SSH shells for worry free usage for students.",
             "github-link":"https://github.com/MOHANKUMAR-IT/Cloud-Based-Virtual-Labs",
-            "site-link":"",
+            "site-link":"/404.html",
         },
         {
-            "img":"http://www.tcedesk.tk:31355/home.png",
-            "title":"Customer Care Registry",
-            "description":"Customer grievance's solving site built using IBM cloud technology and flask framework.",
-            "github-link":"https://github.com/IBM-EPBL/IBM-Project-31539-1660201957"
-        }
+            "img":"/coding-ide.jpg",
+            "title":"Online IDE",
+            "description":"Online Programming Environment for C/C++,Python,Java supported by PHP backend and docker containarized execution for safety.",
+            "github-link":"https://github.com/MOHANKUMAR-IT/IDE",
+            "site-link":"/404.html",
+        },
+        {
+            "img":"/smart-home.jpg",
+            "title":"Smart Home",
+            "description":"Fan and light Toggle control from Android Application supported by IOT sensors.",
+            "github-link":"https://github.com/MOHANKUMAR-IT/Fan-and-Light-control-IOT",
+            "site-link":"/404.html",
+        },        
+        {
+            "img":"/jarvis.png",
+            "title":"J.A.R.V.I.S",
+            "description":"Voice Assistant made using python , selenium framework and voice recognition libraries to act upon voice command.",
+            "github-link":"https://github.com/MOHANKUMAR-IT/Fan-and-Light-control-IOT",
+            "site-link":"/404.html",
+        },    
     ];
     return(
         <div>
             <div className="row p-3">
                 <p className="display-5" style={{fontWeight:'500'}}>My Coding Works</p>
             </div>
-            <div className="row">
-                <div class="col-12">
-                <div className="card-group">
-                { 
-                    projectInfo.map((item)=>{
-                        return(
-                            
-                           
-                                <div className="card m-4" >
-                                    <div class="view overlay">
-                                        <img className="card-img-top img-fluid" src={item['img']} style={{width:'30vw',margin:'auto'}}></img>
-                                        <a href="#!">
-                                        <div class="mask rgba-white-slight"></div>
-                                        </a>
-                                    </div>
-                                    
-                                    <div className="card-header">
-                                        <div className="card-title">
-                                        <p style={{fontWeight:'500'}}>{item['title']}</p>
+            <Row xs={1} md={3} className="g-3">
+                    { 
+                    
+                        projectInfo.map((item)=>{
+                            return(
+                                
+                                <Col>
+                                    <div className="card m-4" style={{height:'63vh'}}>
+                                        <div class="view overlay">
+                                            <img className="card-img-top img-fluid" src={item['img']} style={{margin:'auto',maxHeight:'30vh',objectFit:'conatain'}}></img>
+                                            <a href="#!">
+                                            <div class="mask rgba-white-slight"></div>
+                                            </a>
+                                        </div>
+                                        
+                                        <div className="card-header">
+                                            <div className="card-title">
+                                            <p style={{fontWeight:'500'}}>{item['title']}</p>
+                                            </div>
+                                        </div>
+                                        <div className="card-body">
+                                            <div className="card-tex">
+                                                {item['description']}
+                                            </div>
+                                        </div>
+                                        <div className="card-footer d-flex justify-content-around">
+                                            <a href={item["github-link"]} target="_blank" style={{textDecoration:'none',color:'black',margin:'0',padding:'0'}}>
+                                                <button className="btn btn-primary">Github link</button>
+                                            </a>
+                                            <a href={item['site-link']} target="_blank" style={{textDecoration:'none',color:'black',margin:'0',padding:'0'}}>
+                                                <button className="btn btn-primary">Site link</button>    
+                                            </a>
                                         </div>
                                     </div>
-                                    <div className="card-body">
-                                        <div className="card-tex">
-                                            {item['description']}
-                                        </div>
-                                    </div>
-                                    <div className="card-footer d-flex justify-content-around">
-                                        <a href={item["github-link"]} target="_blank" style={{textDecoration:'none',color:'black',margin:'0',padding:'0'}}>
-                                            <button className="btn btn-primary">Github link</button>
-                                        </a>
-                                        <a href={item['site-link']} target="_blank" style={{textDecoration:'none',color:'black',margin:'0',padding:'0'}}>
-                                            <button className="btn btn-primary">Site link</button>    
-                                        </a>
-                                    </div>
-                                </div>
-                            
-                        );
-                    }
-                )}
-            
-            </div>
-                </div>
-            </div>
-            
-            
-           
+                                </Col>
+                            );
+                        }
+                    )}
+                
+            </Row>              
         </div>
     );
 }
